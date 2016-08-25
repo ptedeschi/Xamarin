66c0dee3809aa12ad8aaa3141f8f9d278120f0e7
Patrick Tedeschi <patrick@tedeschi.com.br>
2016-08-25 09:20:45 -0300

    Sample Xamarin.Forms project to reproduce the issue 43302

---
+++ NavigationPageTitleIssue/NavigationPageTitleIssue/NavigationPageTitleIssue/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NavigationPageTitleIssue
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this.Title = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a pretium nibh. Pellentesque fringilla est non lacinia sollicitudin. Vestibulum pharetra mauris vitae lectus dignissim, vitae auctor libero luctus. Curabitur in";
+        }
+    }
+}
